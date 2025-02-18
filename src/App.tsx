0ce157bcaafc870b4a20cb2bf4d95aaf760c657f
owolabi idowu <owolabiidowu99@gmail.com>
2025-02-18 13:29:51 -0800

    Fix homepage URL

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
